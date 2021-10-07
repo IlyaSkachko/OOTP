356e02e1c6a13cd3e55010a55d628fc28aea162a
Ilya Skachko <ilya.official.03@mail.ru>
2021-10-07 17:10:05 +0300

    first commit

---
+++ Lab2/Lab2/Constructor_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab2
+{
+    public class Constructor_1 : Rectangle
+    {
+        public int height;
+        public int width;
+
+
+
+        public Constructor_1(int height, int width)  // конструтор 1 
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+
+
+        public static void Num1()
+        {
+            int height;
+            int width;
+
+            Console.Write("Введите ширину прямоугольника: ");
+            width = int.Parse(Console.ReadLine());
+            Console.Write("Введите высоту прямоугольника: ");
+            height = int.Parse(Console.ReadLine());
+
+            Constructor_1 rectangle = new Constructor_1(height, width);
+
+            Console.Write($"Площадь прямоугольника = {rectangle.Square(height, width)}\nПериметр прямоугольника = " +
+                    $"{rectangle.Perimetr(height, width)}");
+        }
+
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace Lab2
+{
+    class Program
+    {
+        public static void Menu()
+        {
+            int choice;
+            const int EXIT = 3;
+
+            Constructor_2.Num2();
+            while (true)
+            {
+                Console.WriteLine("\n1. Закрытый конструктор\n2. Конструктор с аргументами\n3. Выход из программы");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Constructor_3.Num3();
+                        break;
+
+                    case 2:
+                        Constructor_1.Num1();
+                        break;
+                    case EXIT:
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.Write("Ошибка ввода!!!");
+                        break;
+                }
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Menu();
+        }
+    }
+}
