404bef6596de7f6e96b3c2cf53d35685a3e93d44
Ilya Skachko <ilya.official.03@mail.ru>
2021-10-14 01:54:05 +0300

    2 commit

---
+++ Lab2/Lab2/Circle.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Lab2
+{
+    public partial class Circle
+    {
+
+        public double square { get; set; }
+        public double length { get; set; }
+        public static int radius { get; set; }
+
+        public readonly int circleID;
+
+
+
+        /*      Статический конструктор        */
+        public static int Radius;
+
+        static Circle()
+        {
+            Radius = 5;
+            Console.WriteLine($"Статический конструктор: \nПлощадь круга = {Square(Radius)} Длина круга = " +
+                $"{Length(Radius)}\n");
+        }
+
+
+        /*      Конструктор с параметрами            */
+
+        public Circle(ref int count)
+        {
+            Console.WriteLine("Введите радиус: ");
+
+            count++;
+            radius = int.Parse(Console.ReadLine());
+            square = Square(radius);
+            length = Length(radius);
+            circleID = GetHashCode();
+
+        }
+
+
+        /*      Закрытый конструктор       */
+        private Circle() { }
+
+
+        /*           переопределение toString()      */
+        public override string ToString()
+        {
+            return $"Площадь: {square}\nДлина: {length}\nХэш: {circleID}\n\n";
+        }
+
+        /*           переопределение GetHashCode()   */
+
+        public override int GetHashCode()
+        {
+            Random rand = new Random();
+            return rand.Next(1, 50);
+        }
+
+
+        /*           переопределение Equals()   */
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType()) return false;
+
+            Circle circle = (Circle)obj;
+            return (this.circleID == circle.circleID);
+        }
+
+
+    }
+
+    public partial class Circle
+    {
+        public static double Square(int radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static double Length(int radius)
+        {
+            return 2 * Math.PI * radius;    
+        }
+    }
+
+}
--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -1,48 +1,76 @@
 ﻿using System;
 
-
 namespace Lab2
 {
-    class Program
+    public class Program
     {
-        public static void Menu()
+
+        public static void info(out int count, out bool res, out Type type)
         {
-            int choice;
-            const int EXIT = 3;
-
-            Constructor_2.Num2();
-            while (true)
+            count = 1;
+            var circles = new Circle[4];
+            for (int i = 0; i < 4; i++)
             {
-                Console.WriteLine("\n1. Закрытый конструктор\n2. Конструктор с аргументами\n3. Выход из программы");
-                choice = int.Parse(Console.ReadLine());
-
-                switch (choice)
-                {
-                    case 1:
-                        Constructor_3.Num3();
-                        break;
-
-                    case 2:
-                        Constructor_1.Num1();
-                        break;
-                    case EXIT:
-                        Environment.Exit(0);
-                        break;
-
-                    default:
-                        Console.Write("Ошибка ввода!!!");
-                        break;
-                }
-
-                Console.ReadLine();
-                Console.Clear();
+                circles[i] = new Circle(ref count);
             }
+
+            foreach (var circle in circles)
+            {
+                Console.WriteLine(circle.ToString());
+            }
+
+            // сравнение
+            res = circles[0].Equals(circles[1]);
+
+            // тип объекта
+            type = circles.GetType();
+
+
+            // Найменьшая и найбольшая площадь
+            minMaxSquare(circles);
+
+        }
+
+        public static void minMaxSquare(Circle[] circles)
+        {
+            double maxSq;
+            double minSq;
+            Circle max = circles[0];
+            Circle min = circles[0];
+            minSq = circles[0].square;
+            maxSq = circles[0].square;
+            for (int i = 0; i < 4; i++)
+            {
+                if (maxSq < circles[i].square)
+                {
+                    max = circles[i];
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (minSq > circles[i].square)
+                {
+                    min = circles[i];
+                }
+            }
+            Console.WriteLine($"\n\nОкружность с найбольшей площадью: {max}\nОкружность с найменьшой площадью: {min}\n\n");
+        }
+
+        public static void privateConstructor()
+        {
+            Circle.radius = 10;
+            Console.WriteLine($"Закрытый конструктор(радиус): {Circle.radius}");
         }
 
 
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Menu();
+            int count;
+            Type type;
+            bool res;
+            info(out count, out res, out type);
+            Console.WriteLine($"\nКоличество объектов: {count}\n\nОкружность 1 равна окружность 2: {res}\n\nТип созданного объекта: {type}\n\n");
+            privateConstructor();
         }
     }
 }
